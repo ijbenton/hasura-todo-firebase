e57b03ed568528f2dde57f4bdf13d3c6a8e1dec0
Ian Benton <bentonian1@gmail.com>
2022-09-18 11:48:41 -0700

    update

--- functions/src/graphql/graphql_client.ts
+++ functions/src/graphql/graphql_client.ts
@@ -1,9 +1,8 @@
-import { GraphQLClient } from "graphql-request";
 import * as functions from "firebase-functions";
+import { GraphQLClient } from "graphql-request";
 
-const url  = functions.config().config.hasura_api_url;
+const url = functions.config().config.hasura_api_url;
 const secret = functions.config().config.hasura_admin_secret;
-
 
 export const client = new GraphQLClient(url, {
   headers: { "x-hasura-admin-secret": secret },
